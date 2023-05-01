46d9d2910f16db8792555a4e8652e2327da254f8
Ala Eddin A. Aldoukhi <75493673+Alaeddin03@users.noreply.github.com>
2023-05-01 11:56:00 +0300

    fixed button issue

--- src/routes/Rooms.jsx
+++ src/routes/Rooms.jsx
@@ -8,18 +8,22 @@ export default function Rooms() {
     <div className='w-1/2 min-w-[200px]'>
         <div className='flex items-center justify-between mb-6'>
         <h1 className='text-center text-2xl'>All Rooms</h1>
-            <button className='btn btn-outline btn-secondary'>
-                <a href='/create'>Create Room</a>
-            </button>
+            <a href='/create' >
+                <button className='btn btn-outline btn-secondary'>
+                    Create Room
+                </button>
+            </a>
         </div>
         {
             rooms.map(room => {
                 return (
                     <div key={room.id} className='flex justify-around items-center gap-3 mb-6 p-3 border border-base-300 rounded-lg max-sm:flex-col'>
                         <h2 className='capitalize text-base'>{room.name}</h2>
-                        <button className='btn btn-outline btn-secondary'>
-                            <a href={`/${room.id}`}>Join Room</a>
-                        </button>
+                        <a href={`/${room.id}`}>
+                            <button className='btn btn-outline btn-secondary'>
+                                Join Room
+                            </button>
+                        </a>
                     </div>
                 )
             }
